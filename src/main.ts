51f2124b2f7fc3c34f9858540cede66d44c1048e
Dave MacGugan <dmagoo@rocketmail.com>
2017-10-29 03:43:11 +0000

    moving files around

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { LostCitiesModule } from './app/lost-cities.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(LostCitiesModule)
   .catch(err => console.log(err));
